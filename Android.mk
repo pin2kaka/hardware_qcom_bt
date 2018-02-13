2e9ccd0a015936df822ecbc61e713748184e00ad
viscropst <bcct2010@live.cn>
2018-02-13 16:48:05 +0800

    Fix msm8992 building
    When I add the bt-vendor in dependencies in msm8992, ckati shows not found this module.
    
    Signed-off-by: xyyx <xyyx@mail.ru>

--- Android.mk
+++ Android.mk
@@ -8,7 +8,7 @@ ifneq ($(BOARD_IS_AUTOMOTIVE),true)
   else
    ifneq ($(filter msm8x27 msm8226 msm8974 msm8960,$(TARGET_BOARD_PLATFORM)),)
      include $(call all-named-subdir-makefiles,msm8960)
-   else ifneq ($(filter msm8994,$(TARGET_BOARD_PLATFORM)),)
+   else ifneq ($(filter msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
      include $(call all-named-subdir-makefiles,msm8992)
    else ifneq ($(wildcard $(LOCAL_PATH)/$(TARGET_BOARD_PLATFORM)),)
      include $(call all-named-subdir-makefiles,$(TARGET_BOARD_PLATFORM))
